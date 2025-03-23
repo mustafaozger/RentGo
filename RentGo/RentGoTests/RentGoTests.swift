f27ea059d36f08cbace1ff1fee491df7320b794e
Eray İnal <114001362+erayinal@users.noreply.github.com>
2025-03-23 19:16:05 +0300

    Initialize Mobile project with Swift and Xcode

---
+++ RentGo/RentGoTests/RentGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RentGoTests.swift
+//  RentGoTests
+//
+//  Created by Eray İnal on 23.03.2025.
+//
+
+import Testing
+@testable import RentGo
+
+struct RentGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
