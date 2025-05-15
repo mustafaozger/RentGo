891823abd9bbb0d078dd5ea7dfe4a7cb0e3c276b
Eray İnal <114001362+erayinal@users.noreply.github.com>
2025-05-15 11:10:07 +0300

    Implement Profile Page for both users and admin

---
+++ Mobile/RentGo/RentGo/View/Admin/AdminProfileTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AdminProfileTableViewCell.swift
+//  RentGo
+//
+//  Created by Eray İnal on 13.05.2025.
+//
+
+import UIKit
+
+class AdminProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Mobile/RentGo/RentGo/View/Profile/ProfilePageTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ProfilePageTableViewCell.swift
+//  RentGo
+//
+//  Created by Eray İnal on 13.05.2025.
+//
+
+import UIKit
+
+class ProfilePageTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var optionsImageView: UIImageView!
+    @IBOutlet weak var optionsNameLabel: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
